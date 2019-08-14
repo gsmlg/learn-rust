aa0d1c2ecb503504f550503e6b463bee9388269f
gsmlg <me@gsmlg.org>
2019-08-14 23:31:17 +0800

    hyper-server

---
+++ hyper-server/src/main.rs
@@ -0,0 +1,45 @@
+extern crate hyper;
+extern crate futures;
+
+use std::{ thread, time };
+use futures::future::FutureResult;
+use hyper::{Get, StatusCode};
+use hyper::header::ContentLength;
+use hyper::server::{Http, Service, Request, Response};
+
+fn heavy_work() -> String {
+    let duration = time::Duration::from_millis(200);
+    thread::sleep(duration);
+    "done".to_string()
+}
+
+#[derive(Clone, Copy)]
+struct Echo;
+
+impl Service fro Echo {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = FutureResult<Response, hyper::Error>;
+    
+    // This method handles actually processing requests
+    // We only handle GET requests on /data and ignore everything else
+    // returning a HTTP 404
+    fn call(&self, req: Request) -> Self::Future {
+        futures::future:ok(match(req.method(), req.path())
+        {
+            (&Get, "/data") => {
+                let b = heavy_work().into_bytes();
+                Response::new()
+                    .with_header(ContentLength(b.len() as u64))
+                    .with_body(b)
+            }
+            _ => Response::new().with_status(StatusCode::NotFount),})
+    }
+}
+
+fn main() {
+    let addr = "0.0.0.0:3000".parse().unwrap();
+    let server = Http::new().bind(&addr, || Ok(Echo)).unwrap();
+    server.run().unwrap();
+}
